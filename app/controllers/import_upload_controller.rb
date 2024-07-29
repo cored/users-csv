b7b8c066cf0d2d0c364ebdc3293f429e7373053f
Rafael George <george.rafael@gmail.com>
2024-07-29 10:06:52 -0600

    ref: rename import_upload_controller

---
+++ app/controllers/import_upload_controller.rb
@@ -0,0 +1,28 @@
+class ImportUploadController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+	def create
+		redirect_to import_upload_index_path, alert: "No file uploaded. Please upload a CSV file." and return if uploaded_file.blank?
+
+		require 'pry'; binding.pry
+
+		@users = Imports::UploadCsv.call(uploaded_file)
+
+		respond_to do |format|
+			format.html { redirect_to import_upload_index_path, notice: "CSV file uploaded successfully." }
+			format.turbo_stream
+		end
+
+	rescue Imports::InvalidCsv => e
+		redirect_to import_upload_index_path, error: "Invalid CSV format: #{e.message}"
+	end
+
+	private
+
+	def uploaded_file
+		params[:file]
+	end
+
+end
